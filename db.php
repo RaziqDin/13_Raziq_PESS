30e50f6aea30a36e0f2cbf87b8f7a71404d210f8
RaziqDin <92512558+RaziqDin@users.noreply.github.com>
2021-10-14 17:17:31 +0800

    First Update
    Upload Files

---
+++ db.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+<?php // define database connections variables as constant
+	
+define('DB_USER',"root"); // db user
+	
+define('DB_PASSWORD',""); // db password
+	
+define('DB_DATABASE',"pessdb"); // db database
+
+define('DB_SERVER',"localhost"); // db server
+
+?>
+</body>
+</html>
---
+++ nav.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+<h1>Police Emergency Service System</h1>
+	
+<nav>
+	
+	<a href="logcall.php">Log Call</a>
+	
+	<a href="Update.php">Update</a>
+	
+	<a href="#">Report</a>
+	
+	<a href="#">History</a>
+</nav>
+</body>
+</html>
